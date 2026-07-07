6b182601cd9f8b96177d7a91d8431948340c7edc
Ramdan Maulana <deltala420@gmail.com>
2026-07-06 22:29:06 -0700

    Create Color.kt

---
+++ app/src/data/presentation/theme/Color.kt
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2026 Mr.Rm19
+ * GitHub: https://github.com/Rm19x
+ * All rights reserved.
+ *
+ * This file is part of the Real-World Smart Home Automation System.
+ * Built with Kotlin & Jetpack Compose for production environment.
+ */
+
+package com.mrrm19.smarthome.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Palet Warna Utama (Tema Terang & Gelap)
+val PrimaryCyber = Color(0xFF00E5FF)       // Cyan neon khas infrastruktur modern
+val PrimaryDarkVibrant = Color(0xFF00B0FF) // Biru tua solid
+
+val DarkBackground = Color(0xFF0B0E14)     // Hitam pekat obsidian untuk kenyamanan mata malam hari
+val DarkSurface = Color(0xFF151A22)        // Abu-abu gelap kontainer komponen
+val DarkSurfaceVariant = Color(0xFF222A36)
+
+val LightBackground = Color(0xFFF4F6F9)    // Putih keabu-abuan bersih untuk siang hari
+val LightSurface = Color(0xFFFFFFFF)       // Putih murni kontainer
+val LightSurfaceVariant = Color(0xFFE9ECEF)
+
+val AccentError = Color(0xFFFF5252)        // Merah peringatan sistem keamanan darurat
+val AccentSuccess = Color(0xFF00E676)      // Hijau indikator hardware terkoneksi
